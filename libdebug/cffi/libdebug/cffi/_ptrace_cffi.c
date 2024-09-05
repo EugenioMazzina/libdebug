07e6fa52aa28a7236fae00d60300ddcdb6c010c5
EugenioMazzina <eugenio.mazzina@mail.polimi.it>
2024-09-05 23:46:31 +0200

    modified:   libdebug/cffi/libdebug/cffi/_ptrace_cffi.c 	modified:   libdebug/cffi/libdebug/cffi/_ptrace_cffi.o 	modified:   libdebug/cffi/ptrace_cffi_source.c

--- libdebug/cffi/libdebug/cffi/_ptrace_cffi.c
+++ libdebug/cffi/libdebug/cffi/_ptrace_cffi.c
@@ -1212,7 +1212,9 @@ int stepping_cont(struct global_state *state, int tid, uint64_t map_start, uint6
         opcode_window = ptrace(PTRACE_PEEKDATA, tid, (void *)current_ip, NULL);
         first_opcode_byte = opcode_window & 0xFF;
 
-        printf("%" PRIu64 "\n", current_ip);
+        if((map_end != map_start) && current_ip < map_end && current_ip > map_start){
+            printf("%" PRIu64 "\n", current_ip);
+        }
 
 
         // if the instruction pointer didn't change, we return
